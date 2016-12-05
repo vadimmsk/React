3a2bcccd02cde9ac86e56b16f1e825bd923a7514
admin <vadimnick@bk.ru>
2016-12-05 17:24:59 +0600

    created second  branch book

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-var image = React.createElement('img', { src: 'https://facebook.github.io/react/img/logo.svg' });
+var image = React.createElement('img', { src: 'https://cdn4.iconfinder.com/data/icons/logos-3/600/React.js_logo-128.png' });
 var title = React.createElement('h1', null, 'Привет React!');
 var subTitle = React.createElement('p', null, 'Библиотека React');
 var container = React.createElement('div', {className: 'container'}, image, title, subTitle);
